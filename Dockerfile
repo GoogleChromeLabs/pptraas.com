9f8ba82ff25b850592aff9cbd815634b416f08ec
Paul Kinlan <paul.kinlan@gmail.com>
2018-01-31 07:57:26 -0800

    Update the docker file so app updates are seperate to infra.

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,20 @@
 FROM kinlan/puppets:latest
+
+# Copy the app
+COPY . /app/
+#COPY local.conf /etc/fonts/local.conf
+WORKDIR app
+RUN npm i
+
+# Add user so we don't need --no-sandbox.
+RUN groupadd -r pptruser && useradd -r -g pptruser -G audio,video pptruser \
+    && mkdir -p /home/pptruser/Downloads \
+    && chown -R pptruser:pptruser /home/pptruser \
+    && chown -R pptruser:pptruser ./node_modules
+
+# Run everything after as non-privileged user.
+USER pptruser
+
+EXPOSE 8084
+ENTRYPOINT ["dumb-init", "--"]
+CMD ["npm", "run", "start"]
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const puppeteer = require('puppeteer');
+
+const randomUUID = require('random-uuid');
 
 app.all('*', function(request, response, next) {
   response.setHeader('Access-Control-Allow-Origin', '*');
@@ -8,40 +11,31 @@ app.all('*', function(request, response, next) {
 });
 
 app.get("/screenshot", async function (request, response) {
-  const puppeteer = require('puppeteer');
-  const randomUUID = require('random-uuid');
   const url = request.query.url;
+
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  const filename = `/tmp/screenshot-${randomUUID()}.png`;
-
-
   const page = await browser.newPage();
   await page.goto(url);
-  await page.screenshot({path: filename});
+  const screenshot = await page.screenshot();
   await browser.close();
 
-  response.sendFile(filename);
+  response.send(screenshot);
 });
 
 app.get("/pdf", async function (request, response) {
-  const puppeteer = require('puppeteer');
-  const randomUUID = require('random-uuid');
+  console.log('pdf')
   const url = request.query.url;
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  const filename = `/tmp/pdf-${randomUUID()}.pdf`;
-
 
   const page = await browser.newPage();
   await page.goto(url);
-  await page.pdf({path: filename});
+  const pdf = await page.pdf();
   await browser.close();
   
-  response.sendFile(filename);
+  response.send(pdf);
 });
 
 app.get("/trace", async function (request, response) {
-  const puppeteer = require('puppeteer');
-  const randomUUID = require('random-uuid');
   const url = request.query.url;
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const filename = `/tmp/trace-${randomUUID()}.json`;
@@ -56,7 +50,6 @@ app.get("/trace", async function (request, response) {
 });
 
 app.get("/test", async function (request, response) {
-  const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   
   await browser.close();
